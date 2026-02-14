43a51c62ed5ed4ce7cac623aedd25105fb472dda
kurminaidu panga <kurminaidu79@gmail.com>
2026-02-14 22:40:41 +0530

    Update EurekaServerProj001Application.java

--- src/main/java/com/ms/eureka/EurekaServerProj001Application.java
+++ src/main/java/com/ms/eureka/EurekaServerProj001Application.java
@@ -9,7 +9,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerProj001Application {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(EurekaServerProj001Application.class, args);
+		System.out.println("runing on jenkins");
 	}
 
 }
